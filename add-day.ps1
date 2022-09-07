f0d4236cf55a50203f309ebf39c6a5c7a3713a48
Arthur Bruel <arthur_bruel@hotmail.com>
2022-09-07 20:58:56 -0300

    feat: update add day script
    Removed year param, repo now works for a single year
    
    Open links to prompt and input, tried to download input, website requires login.

--- add-day.ps1
+++ add-day.ps1
@@ -1,18 +1,21 @@
-param ([int] $day, [int] $year);
-function New-Prompt ($year, $day) {
-  $filename = ".\src\prompts\{0:d4}\{1:d2}.md" -f $year, $day
-  Write-Output $filename
-}
-function New-Input ($year, $day) {
-  $filename = ".\src\inputs\{0:d4}\{1:d2}.txt" -f $year, $day
+param ([int] $day);
+function New-Prompt ($day) {
+  Start-Process "https://adventofcode.com/2021/day/$day"
+  $filename = ".\src\prompts\{0:d2}.md" -f $day
   Write-Output "" > $filename
 }
-function New-Solution ($year, $day) {
-  $filename = ".\src\solutions\{0:d4}\day{1:d2}.rs" -f $year, $day
-  Write-Output $filename
+function New-Input ($day) {
+  Start-Process "https://adventofcode.com/2021/day/$day/input"
+  $filename = ".\src\inputs\{0:d2}.txt" -f $day
+  Write-Output "" > $filename
+}
+function New-Solution ($day) {
+  $filename = ".\src\solutions\day{0:d2}.rs" -f $day
+  Write-Output "" > $filename
 }
 
-git checkout -b "advent-of-code $year day $day" -f $day
-New-Solution $year $day
-New-Input $year $day;
-New-Prompt $year $day;
+git checkout -b ("{0:d4}-{0:d2}" -f $day)
+New-Solution $day;
+New-Input $day;
+New-Prompt $day;
+
