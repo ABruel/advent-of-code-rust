74d3d7e753e4b60255c3e607456b18300cb99c61
Arthur Bruel <arthur_bruel@hotmail.com>
2022-09-07 21:09:36 -0300

    feat: add solve system using cli args
    Add cli args, currently only receives the day to solve

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Day to solve
+    #[clap(short, long, value_parser)]
+    day: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match args.day {
+        _ => unimplemented!("day {}", args.day),
+    }
 }
